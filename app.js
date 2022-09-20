8cbdd84178d41367cfcf9954a046edef89a136b7
José <josesand99v@gmail.com>
2022-09-20 16:04:03 -0300

    Add score system for the game, and then update the web ui to show the score.

--- app.js
+++ app.js
@@ -1,73 +1,5 @@
 //rock paper scissors game
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
-    switch (randomNumber) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-  }
-}
-/*
-function getPlayerChoice(){
-    let playerChoice=null;
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
-}
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
 
-function  playRound(){
-    if (playerChoice === computerChoice){
-        return 'It\'s a draw!';
-    }
-    if (playerChoice === 'rock'){
-        if (computerChoice === 'paper'){
-            return 'You lose! Paper beats rock.';
-        } else {
-            return 'You win! Rock beats scissors.';
-        }
-    }
-    if (playerChoice === 'paper'){
-        if (computerChoice === 'scissors'){
-            return 'You lose! Scissors beats paper.';
-        } else {
-            return 'You win! Paper beats rock.';
-        }
-    }
-    if (playerChoice === 'scissors'){
-        if (computerChoice === 'rock'){
-            return 'You lose! Rock beats scissors.';
-        } else {
-            return 'You win! Scissors beats paper.';
-        }
-    }
-}
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < n; i++){
-        let result = playRound(playerChoice, computerChoice);
-        console.log(result);
-        if (result.includes('win')){
-            playerScore++;
-        } else if (result.includes('lose')){
-            computerScore++;
-        }
-        playerChoice = getPlayerChoice();
-        computerChoice = getComputerChoice();
-    }
-    if (playerScore > computerScore){
-        return 'You win the game!';
-    } else if (playerScore < computerScore){
-        return 'You lose the game!';
-    } else {
-        return 'It\'s a tie game!';
-    }
-}*/
 
 const imgPlayer = document.querySelectorAll(".player__hand");
 console.table(imgPlayer);
@@ -92,12 +24,15 @@ const buttons = document.querySelectorAll(".player__button");
 //add event listener to all buttons
 //when button is clicked, check the class of the button, and display the corresponding image
 //hide all other images
-
-buttons.forEach((button) => {
+let playerChoice = '';
+    buttons.forEach((button) => {
     button.addEventListener('click', () => {
         imgPlayer.forEach((img) => {
             if (img.classList.contains(button.classList[1]) && img.classList.contains('hide')) {
                 img.classList.toggle('hide');
+                playerChoice = button.classList[1];
+
+
             } else if (!img.classList.contains(button.classList[1]) && !img.classList.contains('hide')) {
                 img.classList.toggle('hide');
             }
@@ -106,4 +41,74 @@ buttons.forEach((button) => {
         );
         });
 });
+
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+  }
+}
+let computerChoice = getComputerChoice();
+
+function  playRound(){
+    if (playerChoice === computerChoice){
+        return 0;
+    }
+    if (playerChoice === 'rock'){
+        if (computerChoice === 'paper'){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+    if (playerChoice === 'paper'){
+        if (computerChoice === 'scissors'){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+    if (playerChoice === 'scissors'){
+        if (computerChoice === 'rock'){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
+const number = document.querySelectorAll(".number");
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    while(true){
+        let roundResult = playRound();
+        if (roundResult === 1){
+            playerScore++;
+            number.forEach((num) => {
+                if (num.classList.contains('player__score')){
+                    num.textContent = playerScore;
+                }
+            });
+
+        }else if (roundResult === -1){
+            computerScore++;
+            number.forEach((num) => {
+                if (num.classList.contains('machine__score')){
+                    num.textContent = computerScore;
+                }
+            }
+            );
+        }
+        
+        playerChoice = getPlayerChoice();
+        computerChoice = getComputerChoice();
+    }
+}
+game();
 
