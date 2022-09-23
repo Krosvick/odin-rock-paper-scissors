4b04e3e9507ea73f11ad457d4d9959b19a8d125f
José <josesand99v@gmail.com>
2022-09-23 20:36:39 -0300

    Finish first version of rock paper scissors ui plus game logic
    The game work as intended but the ui is unfinished and there is a lot
    messy code that should be refactored

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 const imgPlayer = document.querySelectorAll(".player__hand");
 const imgMachine = document.querySelectorAll(".machine__hand");
+const resultScreen = document.querySelector('#result');
 
 //hide all imgplayer and imgmachine that doesnt have the class #default on load event listener//
 window.addEventListener('load', () => {
@@ -18,6 +19,7 @@ window.addEventListener('load', () => {
         }
     }
     );
+    resultScreen.style.display = 'none';
 });
 
 function getComputerChoice(){
@@ -71,11 +73,14 @@ let playerScore = 0;
 let computerScore = 0;
 const number = document.querySelectorAll(".number");
 function game(){
+    console.log(number);
     playerScore = number[0].textContent;
     computerScore = number[1].textContent;
     let roundResult = playRound();
     if (roundResult === 1){
+        console.log("before" + playerScore);
         playerScore++;
+        console.log("after" + playerScore);
         number.forEach((num) => {
             if (num.classList.contains('score__player')){
                 num.textContent = playerScore++;
@@ -83,7 +88,9 @@ function game(){
         });
 
     }else if (roundResult === -1){
+        console.log("before" + computerScore);
         computerScore++;
+        console.log("after" + computerScore);
         number.forEach((num) => {
             if (num.classList.contains('score__machine')){
                 num.textContent = computerScore++;
@@ -100,9 +107,6 @@ const buttons = document.querySelectorAll(".player__button");
 let playerChoice = '';
     buttons.forEach((button) => { //literally thats the whole function of this chunk of code
     button.addEventListener('click', () => {
-        if(playerScore <= 5 && computerScore <= 5){
-            game();
-        }
         imgPlayer.forEach((img, index) => {
             if(!img.classList.contains('hide')){
                 img.classList.toggle('hide');
@@ -120,10 +124,55 @@ let playerChoice = '';
                 playerChoice = 'scissors';
             }
             return playerChoice;
-        });
-
             
         });
+            if(playerScore < 6 || computerScore < 6){
+                game();
+                }
+            if (playerScore == 6){
+                resultScreen.style.display = 'flex';
+                //set p inside resultScreen to "You win!"
+                resultScreen.childNodes[1].textContent = "You win!";
+            }
+            else if (computerScore == 6){
+                resultScreen.style.display = 'flex';
+                resultScreen.childNodes[1].textContent = "You lose!";
+
+            }
+        });
+
         
     });
+const resultBtn = document.querySelector('.result_btn');
+resultBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    number.forEach((num) => {
+        if (num.classList.contains('score__player')){
+            num.textContent = playerScore;
+        }
+        if (num.classList.contains('score__machine')){
+            num.textContent = computerScore;
+        }
+    }
+    );
+    resultScreen.style.display = 'none';
+    imgPlayer.forEach((img) => {
+        if (!img.classList.contains('hide')){
+            img.classList.toggle('hide');
+        }
+        if (img.classList.contains('default')){
+            img.classList.toggle('hide');
+        }
+    });
+    imgMachine.forEach((img) => {
+        if (!img.classList.contains('hide')){
+            img.classList.toggle('hide');
+        }
+        if (img.classList.contains('default')){
+            img.classList.toggle('hide');
+        }
+    }
+    );
+});
 
